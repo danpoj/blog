1e804f2da843f53d7c4a9976df09cca8781b2682
danpoj <jws970306@khu.ac.kr>
2023-11-20 13:32:47 +0900

    add robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://danpoj.pages.dev/sitemap.xml',
+    host: 'https://danpoj.pages.dev',
+  }
+}
