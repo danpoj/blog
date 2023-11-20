4f69b3279b64d26d3d349d5d7fc9fea99f3e0783
danpoj <jws970306@khu.ac.kr>
2023-11-21 02:29:25 +0900

    add posts

--- app/page.tsx
+++ app/page.tsx
@@ -4,39 +4,29 @@ export default function Page() {
   return (
     <div className='space-y-4'>
       <h1 className='font-medium text-2xl mb-8 tracking-tighter'>홈</h1>
-      <h2 className='text-sm'>
-        안녕하세요 👋 <br /> Web Front-End 개발자 장원석입니다
-      </h2>
-      <div className='flex flex-col text-sm'>
-        <h3 className='uppercase mb-1'>contact</h3>
-        <p>
-          <span className='pr-2'>이메일:</span> danpoj123@gmail.com
-        </p>
-        <p>
-          <span className='pr-2'>전화번호:</span> 010-2477-8664
-        </p>
-      </div>
-      <div className='space-y-2'>
-        <a
-          href='https://github.com/danpoj'
-          target='_blank'
-          rel='noreferrer noopener'
-          className='text-blue-700 underline underline-offset-4 block w-fit hover:opacity-80'
-        >
-          github
-        </a>
-        <a
-          href='/resume/resume.pdf'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='text-blue-700 underline underline-offset-4 block w-fit hover:opacity-80'
-        >
-          resume
-        </a>
-      </div>
 
-      <div className='aspect-video bg-gradient-to-r from from-black via-black/90 to-black/80 pt-4 pb-8 px-6 flex flex-col justify-between rounded-lg max-w-[36rem]'>
-        <span className='text-8xl sm:text-9xl text-white font-bold'>D</span>
+      <div className='aspect-video bg-gradient-to-r from from-black via-black/90 to-black/80 pt-4 pb-8 px-6 flex flex-col justify-between rounded-lg max-w-[28rem]'>
+        <div className='text-white tracking-tight space-y-2'>
+          <p>danpoj123@gmail.com</p>
+          <div className='flex gap-2'>
+            <a
+              href='https://github.com/danpoj'
+              target='_blank'
+              rel='noreferrer noopener'
+              className='underline underline-offset-4 block w-fit hover:opacity-80'
+            >
+              github
+            </a>
+            <a
+              href='/resume/resume.pdf'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='underline underline-offset-4 block w-fit hover:opacity-80'
+            >
+              resume
+            </a>
+          </div>
+        </div>
         <div className='flex items-center gap-4'>
           <Image
             unoptimized
@@ -46,7 +36,7 @@ export default function Page() {
             height={60}
             className='rounded-lg'
           />
-          <span className='text-white font-black text-2xl'>DanpoJ</span>
+          <span className='text-white font-bold tracking-tighter text-2xl'>DanpoJ</span>
         </div>
       </div>
     </div>
